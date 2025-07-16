95f6ea812a90e83cbf6f6cd5243e3a6ba44b0a0a
NahomTesM <nahomteshome19@gmail.com>
2025-07-16 10:00:29 +0300

    add python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+sum = number1 + number2
+diff = number1 - number2
+product = number2 * number1
+
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {diff}")
+print(f"Multiplication of {number1} and {number2} is {product}")
