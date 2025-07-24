57e5c7d3044d83c57921f7a5c577930cede2ff65
NahomTesM <nahomteshome19@gmail.com>
2025-07-24 11:54:20 +0300

    add arithmetic_operations

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -5,7 +5,7 @@ def display_current_datetime():
     global current_date 
     current_date = datetime.now()
 
-    print(f"Current date and time: {current_date}")
+    print(f"Current date and time: {current_date.strftime("%Y-%m-%d %H:%M:%S")}")
 
 def calculate_future_date():
     added_date = int(input("Enter the number of days to add to the current date: "))
